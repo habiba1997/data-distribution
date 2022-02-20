5896184dd26631abdc33eac0ad299a2c731978ab
Habiba.Ahmed <habiba.hamad97@eng-st.cu.edu.eg>
2022-02-21 00:12:55 +0200

    editing services

--- patients/src/main/java/com/data/distribution/patients/controller/DataController.java
+++ patients/src/main/java/com/data/distribution/patients/controller/DataController.java
@@ -1,9 +1,13 @@
 package com.data.distribution.patients.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DataController {
@@ -11,8 +15,12 @@ public class DataController {
 	@Autowired
 	private Environment environment;
 
+	@Value("${spring.application.name}")
+	private String appName;
+
 	@GetMapping("/data")
-	public String getData() {
-		return environment.getProperty("data.key.value");
+	public String checkStatus() {
+		return "Checking status ...... " + " " + appName + " running on port " +
+				environment.getProperty("local.server.port");
 	}
 }
